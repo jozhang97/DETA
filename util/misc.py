fd121aaa05f471e0ffb27ee2074defb093e4adef
Jang Hyun Cho <jc88964@dgx-4.cs.utexas.edu>
2022-12-22 00:42:55 -0600

    fix version check lines

--- util/misc.py
+++ util/misc.py
@@ -27,7 +27,8 @@ from torch import Tensor
 
 # needed due to empty tensor bug in pytorch and torchvision 0.5
 import torchvision
-if float(torchvision.__version__[:3]) < 0.5:
+if float(torchvision.__version__.split('.')[0]) == 0 \
+and float(torchvision.__version__.split('.')[1]) < 5:
     import math
     from torchvision.ops.misc import _NewEmptyTensorOp
     def _check_size_scale_factor(dim, size, scale_factor):
@@ -54,7 +55,8 @@ if float(torchvision.__version__[:3]) < 0.5:
         return [
             int(math.floor(input.size(i + 2) * scale_factors[i])) for i in range(dim)
         ]
-elif float(torchvision.__version__[:3]) < 0.7:
+elif float(torchvision.__version__.split('.')[0]) == 0 \
+and float(torchvision.__version__.split('.')[1]) < 7:
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
